c5947565d2c018f8fb6cd2a69be534fabc22be3c
sasi-kumar-1 <kaluvaisasikumar59260@gmail.com>
2023-01-03 21:03:43 +0530

    done

---
+++ javascript2/OOP/Four.js
@@ -0,0 +1,13 @@
+class Account {
+    min_Bal = 500
+    open_Account() { }
+    deposit_Amount() { }
+    get_Bal() { }
+    get_St() { }
+    withdrawl() { }
+    close_account() { }
+}
+let a1 = new Account()
+let a2 = new Account()
+console.log(a1)
+console.log(a2)
