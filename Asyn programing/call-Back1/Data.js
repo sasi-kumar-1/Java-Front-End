47fa34840d28aab43c70c2ad2a519905772c0adf
sasi-kumar-1 <kaluvaisasikumar59260@gmail.com>
2023-01-12 14:25:13 +0530

    done

---
+++ Asyn programing/call-Back1/Data.js
@@ -0,0 +1,23 @@
+let employees = [{ id: 101, name: "Rahul", email: "rahul@pm.com" },
+{ id: 102, name: "Sonia", email: "sonia@pm.com" }]
+
+let createEmployee = (emp, callback) => {
+    setTimeout(() => {
+        employees.push(emp)
+        callback()
+    }, [4000])
+}
+let getEmployees = () => {
+    setTimeout(() => {
+        let rows = ""
+        employees.map((employee) => {
+            rows = rows + `<tr>
+                            <td>${employee.id}</td>
+                            <td>${employee.name}</td>
+                            <td>${employee.email}</td>
+                           </tr>`
+        })
+        document.getElementById('tbody_Data').innerHTML = rows
+    }, [1000])
+}
+createEmployee({ id: 103, name: "Priyanka", email: "priya@gmail.com" }, getEmployees)
---
+++ Asyn programing/call-Back1/Data1.js
@@ -0,0 +1,13 @@
+let employees = [{ id: 101, name: "Rahul", email: "rahul@pm.com" },
+{ id: 102, name: "Sonia", email: "sonia@pm.com" }]
+
+let createEmployee = () => {
+    setTimeout(() => {
+
+    }, [4000])
+}
+let getEmployees = () => {
+    setTimeout(() => {
+
+    }, [1000])
+}
