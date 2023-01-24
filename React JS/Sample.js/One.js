79e140f7e249465c0d8253b3df5bb13ad7189bc3
sasi-kumar-1 <kaluvaisasikumar59260@gmail.com>
2023-01-24 13:07:36 +0530

    done

---
+++ React JS/Sample.js/One.js
@@ -0,0 +1,5 @@
+let product = {}
+console.log(product.naya)
+
+let emps = []
+emps.length > 0 ? console.log("Not empty") : console.log("Empty")
---
+++ React JS/Sample.js/Two.js
@@ -0,0 +1,2 @@
+let emp = { id: 101, name: "sasikumar", sal: 45000 }
+console.log(Object.keys(emp).length)
