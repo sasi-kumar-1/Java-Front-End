8d11329b865446143422ae9f20513528bb23572c
sasi-kumar-1 <kaluvaisasikumar59260@gmail.com>
2023-02-09 10:51:17 +0530

    done

--- React JS/REACT AS/Index-1.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom'
-//ReactDOM.render("Hello,GA", document.getElementById('vali'));
-
-ReactDOM.render(<h1>Hello,Good Afternoon</h1>, document.getElementById('vali'))
-//"What you want to render", "Where you want to render"
-//ReactDOM.render(1,2)
--- React JS/REACT AS/Index.js
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom'
-import App from './App'
-
-ReactDOM.render(<App />, document.getElementById('vali'))
-//ReactDOM.render("Hello", document.getElementById('vali'))
